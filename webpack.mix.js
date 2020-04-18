3bbeaae6937d91e87a0163c9a230d7dba811ebda
Andre Madarang <andre_madarang@hotmail.com>
2020-04-18 19:19:13 -0400

    Part 9 - PurgeCSS and Deployment

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/main.css', 'public/css', [
-        require('tailwindcss'),
-    ])
+mix.postCss('resources/css/main.css', 'public/css', [
+    require('tailwindcss'),
+])
+.purgeCss();
