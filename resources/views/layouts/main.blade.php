1e8548b6eb9631475c881c4a0d9ea49a150917f2
Andre Madarang <andre_madarang@hotmail.com>
2020-04-19 00:53:05 -0400

    Attribute for TMDb

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,6 +39,11 @@
         </div>
     </nav>
     @yield('content')
+    <footer class="border border-t border-gray-800">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+        </div>
+    </footer>
     <livewire:scripts>
     @yield('scripts')
 </body>
